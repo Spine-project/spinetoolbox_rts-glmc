ee2cb68b4c7be2f6066a9d65f2527fc91622c911
nnhjy <jiangyi.huang@vtt.fi>
2020-07-17 18:00:10 +0300

    implement piecewise heat_rate/flow_efficiency to the corresponding units

--- spine_rts-gmlc/Spine_RTS-GMLC.jl
+++ spine_rts-gmlc/Spine_RTS-GMLC.jl
@@ -75,7 +75,7 @@ end
 
 # access defined object and parameters
 ## demand(node = node_group__node()[1][:node1], t = time_slice()[1])
-## demand(node=node()[first(findall(n->n.name == :node_name_here, node()))])
+## demand(node=node()[first(findall(n->n.name == :node_name_here/Symbol("node_name_here"), node()))])
 
 ## key = m.ext[:constraints][:constraint_name].keys[3]
 ## m.ext[:constraints][:constraint_name][key]
